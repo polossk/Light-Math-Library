3a12eebc677f3a1f50dbaf1e4554a1c44ab12403
Shangkun Shen <polossk@126.com>
2019-02-17 23:33:49 +0800

    Update Dense.hpp

--- Dense.hpp
+++ Dense.hpp
@@ -2,3 +2,17 @@
 
 #include "base.hpp"
 #include "Shape.hpp"
+#include "Exp.hpp"
+
+namespace lmlib
+{
+	template <typename Container, int dimension, typename DType>
+	struct TRValue : public expr::RValueExp<Container, DType> {};
+
+	template <typename Saver, typename RValue, int dim,
+		typename DType, typename ExpType, int etype>
+		inline void MapExp(TRValue<RValue, dim, DType> *dst, const expr::Exp<ExpType, DType, etype> &exp);
+
+}
+
+
