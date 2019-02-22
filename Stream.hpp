346f4c52ed0c81b09a9159a41f0f5a15e9473aae
Shangkun Shen <polossk@126.com>
2019-02-22 16:33:10 +0800

    Update Stream.hpp

--- Stream.hpp
+++ Stream.hpp
@@ -1,12 +1,12 @@
-#pragma once
+#ifndef LIGHT_MATH_LIBRARY_STREAM_HPP
+#define LIGHT_MATH_LIBRARY_STREAM_HPP
 
 #include "base.hpp"
 
-namespace lmlib
-{
-	struct Stream
-	{
-		inline void Wait() {}
-		inline bool CheckIdle() { return true; }
-	};
+namespace lmlib {
+struct Stream {
+  inline void Wait() {}
+  inline bool CheckIdle() { return true; }
+};
 } // namespace lmlib
+#endif // LIGHT_MATH_LIBRARY_STREAM_HPP
