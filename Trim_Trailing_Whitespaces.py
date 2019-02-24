84a700d7cc0fde5c20405578510db8fb01129917
Shangkun Shen <polossk@126.com>
2019-02-24 19:35:15 +0800

    Create Trim_Trailing_Whitespaces.py

---
+++ Trim_Trailing_Whitespaces.py
@@ -0,0 +1,30 @@
+import glob
+
+DEFAULT_CODE_FILE_EXT = [
+    "*.cpp", "*.cc", "*.hpp",
+    "*.c", "*.h",
+    "*.py"
+]
+
+
+def deal(filename):
+    text_data = []
+    with open(filename, 'r', encoding='UTF-8') as fin:
+        text_data = fin.readlines()
+    text_data = [_.rstrip() for _ in text_data]
+    with open(filename, "w", encoding='UTF-8') as fout:
+        for line in text_data:
+            print(line, file=fout)
+    print(filename, "done.")
+
+
+def main():
+    work_list = []
+    for ext in DEFAULT_CODE_FILE_EXT:
+        work_list += glob.glob(ext)
+    for work in work_list:
+        deal(work)
+
+
+if __name__ == "__main__":
+    main()
