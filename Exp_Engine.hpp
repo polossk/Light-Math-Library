ebd3b9209d60f68cea6d6fa39760f073ee33670d
Shangkun Shen <polossk@126.com>
2019-03-14 21:33:06 +0800

    add Exp_Engine.hpp

---
+++ Exp_Engine.hpp
@@ -0,0 +1,8 @@
+#ifndef LMLIB_EXP_ENGINE_HPP_
+#define LMLIB_EXP_ENGINE_HPP_
+
+#include "LMBase.hpp"
+
+namespace lmlib {} // namespace lmlib
+
+#endif // LMLIB_  EXP_ENGINE_HPP_
--- lmlib.h
+++ lmlib.h
@@ -2,5 +2,6 @@
 #define LMLIB_lmlin_HPP_
 
 #include "Dense.hpp"
+#inlucde "Exp_Engine.hpp"
 
 #endif // LMLIB_lmlin_HPP_
