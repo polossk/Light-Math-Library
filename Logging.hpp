33b4cef66159291ed4f24d8700ffe03e19afb535
Shangkun Shen <polossk@126.com>
2019-03-21 09:25:53 +0800

    Create Logging.hpp

---
+++ Logging.hpp
@@ -0,0 +1,12 @@
+#ifndef LMLIB_LOGGING_HPP_
+#define LMLIB_LOGGING_HPP_
+
+#include <cstdio>
+#include <cstdlib>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "./LMBase.hpp"
+
+#endif // LMLIB_LOGGING_HPP_
