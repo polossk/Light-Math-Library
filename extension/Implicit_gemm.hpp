1a1917eab30dc6c34277c4a14888c0bbc0a87487
Shangkun Shen <polossk@126.com>
2019-04-10 20:27:10 +0800

    Create Implicit_gemm.hpp

---
+++ extension/Implicit_gemm.hpp
@@ -0,0 +1,10 @@
+#ifndef LMLIB_EXTENSION_IMPLICIT_GEMM_HPP_
+#define LMLIB_EXTENSION_IMPLICIT_GEMM_HPP_
+
+namespace lmlib {
+
+namespace expr {} // namespace expr
+
+} // namespace lmlib
+
+#endif // LMLIB_EXTENSION_IMPLICIT_GEMM_HPP_
