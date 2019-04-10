6cc0a9046fd9bdde74d32616351c10625af77fcf
Shangkun Shen <polossk@126.com>
2019-04-10 20:27:20 +0800

    Create Dot_Engine.hpp

---
+++ Dot_Engine.hpp
@@ -0,0 +1,9 @@
+#ifndef LMLIB_DOT_ENGINE_HPP_
+#define LMLIB_DOT_ENGINE_HPP_
+
+#include <vector>
+
+#include "./LMBase.hpp"
+#include "./extension/Implicit_gemm.hpp"
+
+#endif // LMLIB_DOT_ENGINE_HPP_
